1f94911b47b708fa25103f71aab46c93d60ec7aa
Gustavo <gustavow077@gmail.com>
2026-07-06 13:36:56 -0300

    fix: move toast to top of screen, no longer blocks form buttons

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -32,7 +32,7 @@ export function ToastProvider({ children }: { children: ReactNode }) {
   return (
     <ToastContext.Provider value={{ toast }}>
       {children}
-      <div className="fixed bottom-24 left-1/2 -translate-x-1/2 z-[100] flex flex-col gap-2 items-center pointer-events-none">
+      <div className="fixed top-4 left-1/2 -translate-x-1/2 z-[100] flex flex-col gap-2 items-center pointer-events-none">
         {toasts.map((t) => (
           <div
             key={t.id}
